ab7d7269d7c7c69ce6cd588a741cefdde1b7e8a8
Mir Mustafa Ali Razvi <mirmustafaalirazvi2006@gmail.com>
2026-01-15 12:41:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,37 +12,3 @@ function validate(){
   alert("Thank you " + name);
  }
 }
-
-/* SHOW ALL SECTIONS SAFELY */
-document.addEventListener("DOMContentLoaded", () => {
- document.querySelectorAll("section").forEach(sec=>{
-  sec.style.opacity="1";
-  sec.style.transform="none";
- });
-});
-
-/* BACK TO TOP */
-let topBtn = document.getElementById("topBtn");
-
-window.addEventListener("scroll", () => {
-
- if(!topBtn) return;
-
- if(document.body.scrollTop > 200 || 
-    document.documentElement.scrollTop > 200){
-  topBtn.style.display="block";
- }
- else{
-  topBtn.style.display="none";
- }
-
-});
-
-if(topBtn){
- topBtn.onclick = function(){
-  window.scrollTo({
-   top:0,
-   behavior:"smooth"
-  });
- }
-}
